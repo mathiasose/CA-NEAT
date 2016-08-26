4b12aeb2b8360ef89e9574ae530318511eeafac6
Mathias Ose <mathias.ose@gmail.com>
2016-08-26 23:11:07 +0200

    Add one_max and rule90 problems

---
+++ problems/rule90.py
@@ -0,0 +1,61 @@
+import os
+from datetime import datetime
+
+from run_experiment import initialize_scenario
+from selection import fitness_proportionate
+from utils import mutate, random_bitstring, splice
+
+
+def geno_to_pheno(genotype):
+    return {bin(i).lstrip('0b').rjust(3, '0'): b for i, b in enumerate(genotype)}
+
+
+def fitness(phenotype):
+    def develop(world):
+        world = '0' + world + '0'
+        for i, b in enumerate(world):
+            if i == 0:
+                yield '0' + world[:2]
+            elif i == len(world) - 1:
+                yield world[-2:] + '0'
+            else:
+                yield world[i - 1:i + 2]
+
+    rule90 = {bin(i).lstrip('0b').rjust(3, '0'): b for i, b in enumerate('01011010')}
+    world = benchmark = '010'
+
+    for _ in range(15):
+        benchmark = ''.join(map(rule90.get, develop(benchmark)))
+        world = ''.join(map(phenotype.get, develop(world)))
+
+    return sum(a == b for a, b in zip(world, benchmark)) / len(benchmark)
+
+
+if __name__ == '__main__':
+    POPULATION_SIZE = 10
+    INITIAL_GENOTYPES = tuple(random_bitstring(8) for _ in range(POPULATION_SIZE))
+    MUTATION_CHANCE = 0.01
+    GENO_TO_PHENO_F = geno_to_pheno
+    FITNESS_F = fitness
+    SELECTION_F = fitness_proportionate
+    CROSSOVER_F = splice
+    MUTATION_F = mutate
+
+    DB_DIR = 'db/'
+    DB_PATH = os.path.join('sqlite:///', DB_DIR, '{}.db'.format(datetime.now()))
+
+    for n_generations in (100,):
+        description = '"Rule 90 1D CA", population size: {}, generations: {}'.format(POPULATION_SIZE, n_generations)
+        initialize_scenario(
+            db_path=DB_PATH,
+            description=description,
+            initial_genotypes=INITIAL_GENOTYPES,
+            population_size=POPULATION_SIZE,
+            generations=n_generations,
+            geno_to_pheno_f=GENO_TO_PHENO_F,
+            fitness_f=FITNESS_F,
+            selection_f=SELECTION_F,
+            crossover_f=CROSSOVER_F,
+            mutation_f=MUTATION_F,
+            mutation_chance=MUTATION_CHANCE,
+        )
--- run_experiment.py
+++ run_experiment.py
@@ -119,33 +119,3 @@ def handle_individual(db_path, scenario_id, generation, individual_number, genot
     get_db(db_path).save_individual(individual)
 
     return individual.genotype
-
-
-if __name__ == '__main__':
-    POPULATION_SIZE = 10
-    INITIAL_GENOTYPES = tuple(random_bitstring(16) for _ in range(POPULATION_SIZE))
-    MUTATION_CHANCE = 0.1
-    GENO_TO_PHENO_F = lambda genotype: genotype
-    FITNESS_F = lambda phenotype: sum(c == '1' for c in phenotype)
-    SELECTION_F = fitness_proportionate
-    CROSSOVER_F = splice
-    MUTATION_F = mutate
-
-    DB_DIR = 'db/'
-    DB_PATH = os.path.join('sqlite:///', DB_DIR, '{}.db'.format(datetime.now()))
-
-    for n_generations in (10, 50, 100):
-        description = '"Max ones", population size: {}, generations: {}'.format(POPULATION_SIZE, n_generations)
-        initialize_scenario(
-            db_path=DB_PATH,
-            description=description,
-            initial_genotypes=INITIAL_GENOTYPES,
-            population_size=POPULATION_SIZE,
-            generations=n_generations,
-            geno_to_pheno_f=GENO_TO_PHENO_F,
-            fitness_f=FITNESS_F,
-            selection_f=SELECTION_F,
-            crossover_f=CROSSOVER_F,
-            mutation_f=MUTATION_F,
-            mutation_chance=MUTATION_CHANCE,
-        )
