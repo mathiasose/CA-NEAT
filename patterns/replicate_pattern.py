4b627338296fcbd660cae646a1d5b1cc784513a6
Mathias Ose <mathias.ose@gmail.com>
2016-11-16 20:49:28 +0100

    Fix error in replication eval
    Literally a corner case

--- patterns/replicate_pattern.py
+++ patterns/replicate_pattern.py
@@ -5,19 +5,20 @@ from geometry.cell_grid import CellGrid2D, FiniteCellGrid2D
 
 
 def count_pattern(grid: CellGrid2D, pattern) -> int:
-    live_cells = grid.get_live_cells()
+    (x_min, y_min), (x_max, y_max) = grid.get_extreme_coords(pad=1)
 
     pattern_h, pattern_w = len(pattern), len(pattern[0])
 
     count = 0
-    for ((x, y), _) in live_cells:
-        rectangle = grid.get_rectangle(
-            x_range=(x - 1, x - 1 + pattern_w),
-            y_range=(y - 1, y - 1 + pattern_h)
-        )
+    for y in range(y_min, y_max):
+        for x in range(x_min, x_max):
+            rectangle = grid.get_rectangle(
+                x_range=(x, x + pattern_w),
+                y_range=(y, y + pattern_h)
+            )
 
-        if rectangle == pattern:
-            count += 1
+            if rectangle == pattern:
+                count += 1
 
     return count
 
@@ -33,48 +34,46 @@ def count_correct_cells(test_pattern, target_pattern) -> int:
 
 
 def find_pattern_partial_matches(grid: CellGrid2D, pattern) -> Iterator[float]:
-    live_cells = set(coord for coord, _ in grid.get_live_cells())
+    (x_min, y_min), (x_max, y_max) = grid.get_extreme_coords(pad=1)
 
     pattern_h, pattern_w = len(pattern), len(pattern[0])
     pattern_area = pattern_h * pattern_w
 
-    for (x, y) in live_cells:
-        x_range = (x - 1, x - 1 + pattern_w)
-        y_range = (y - 1, y - 1 + pattern_h)
-        rectangle = grid.get_rectangle(
-            x_range=x_range,
-            y_range=y_range,
-        )
+    for y in range(y_min, y_max):
+        for x in range(x_min, x_max):
+            rectangle = grid.get_rectangle(
+                x_range=(x, x + pattern_w),
+                y_range=(y, y + pattern_h),
+            )
 
-        correct_count = count_correct_cells(test_pattern=rectangle, target_pattern=pattern)
+            if all(all(x == grid.dead_cell for x in row) for row in rectangle):
+                yield 0.0
 
-        yield (correct_count / pattern_area)
+            correct_count = count_correct_cells(test_pattern=rectangle, target_pattern=pattern)
+
+            yield (correct_count / pattern_area)
 
 
 if __name__ == '__main__':
     cell_states = (' ', '■', '□', '▨')
     grid = FiniteCellGrid2D(cell_states=cell_states, x_range=(-10, 20), y_range=(-10, 20))
     pattern = (
-        (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '□', '□', '▨', '□', '□', '□', '□', ' ',),
-        (' ', '▨', '▨', '▨', '▨', '▨', '▨', '▨', ' ',),
-        (' ', '□', '□', '▨', '□', '□', '□', '□', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+        (' ', ' ', '■', ' ', '■', ' ', ' ',),
+        (' ', '■', ' ', '■', ' ', '■', ' ',),
+        (' ', ' ', '■', ' ', '■', ' ', ' ',),
+        (' ', '■', ' ', '■', ' ', '■', ' ',),
+        (' ', ' ', '■', ' ', '■', ' ', ' ',),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' ',),
     )
     wrong_pattern = (
-        (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '▨', '▨', '▨', '▨', '▨', '▨', '▨', ' ',),
-        (' ', '□', '□', '▨', '□', '□', '□', '□', ' ',),
-        (' ', '▨', '▨', '▨', '▨', '▨', '▨', '▨', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', '■', '□', '▨', '□', '■', '■', '■', ' ',),
-        (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+        (' ', ' ', '■', ' ', '■', ' ', ' ',),
+        (' ', '■', ' ', '■', ' ', '■', ' ',),
+        (' ', ' ', '■', '■', '■', ' ', ' ',),
+        (' ', '■', ' ', '■', ' ', '■', ' ',),
+        (' ', ' ', '■', ' ', '■', ' ', ' ',),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' ',),
     )
 
     grid.add_pattern_at_coord(pattern, (0, 0))
