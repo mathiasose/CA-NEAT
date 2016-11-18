dc032d9aa9816cd50bab6271afc9c33ac6bbfd18
Mathias Ose <mathias.ose@gmail.com>
2016-11-18 22:06:46 +0100

    Update plot_fitness.py

--- visualization/plot_fitness.py
+++ visualization/plot_fitness.py
@@ -13,11 +13,13 @@ def plot_fitnesses_over_generations(db: Db, scenario_id: int, title=None, interv
 
     assert scenario
 
-    n_generations = scenario.generations
-
-    plt.ion()
     generation_fitnesses = {}
     n_species = {}
+
+    if action == 'show':
+        plt.ion()
+    else:
+        plt.ioff()
 
     def draw():
         plt.clf()
@@ -27,30 +29,33 @@ def plot_fitnesses_over_generations(db: Db, scenario_id: int, title=None, interv
         plt.title(title or scenario.description)
         # fig.canvas.set_window_title(os.path.basename(db_path))
 
-        ax1.axis([0, max(generation_fitnesses.keys()), -0.1, 1.1])
+        last_gen = max(generation_fitnesses.keys()) + 1
+        ax1.axis([0, last_gen, -0.1, 1.1])
         ax1.boxplot(
-            x=tuple(generation_fitnesses.get(n, []) for n in range(0, n_generations)),
-            positions=range(0, n_generations),
-            labels=tuple(i if i % 10 == 0 else '' for i in range(0, n_generations)),
+            x=tuple(generation_fitnesses.get(n, []) for n in range(0, last_gen)),
+            positions=range(0, last_gen),
+            labels=tuple(i if i % 10 == 0 else '' for i in range(0, last_gen)),
         )
         ax1.set_ylabel('fitness')
         ax1.set_xlabel('generation')
 
         ax2 = ax1.twinx()
-        ax2.axis([0, n_generations, 0, max(n_species.values()) + 1])
+        ax2.axis([0, last_gen, 0, max(n_species.values()) + 1])
         ax2.plot(
-            range(0, n_generations),
-            tuple(n_species.get(n, 0) for n in range(0, n_generations)),
+            range(0, last_gen),
+            tuple(n_species.get(n, 0) for n in range(0, last_gen)),
             'go'
         )
         ax2.set_ylabel('number of species')
 
         return fig
 
+    done = 0
     while True:
-        for generation_n in range(n_generations):
+        for generation_n in range(done, scenario.generations):
             generation = db.get_generation(scenario_id=scenario_id, generation=generation_n, session=session)
             if generation.count() == 0:
+                done = generation_n - 2
                 break
 
             genotypes = pluck(generation, 'genotype')
@@ -77,8 +82,8 @@ def plot_fitnesses_over_generations(db: Db, scenario_id: int, title=None, interv
 
 
 if __name__ == '__main__':
-    problem_dir = 'replicate_norwegian_flag/'
-    db_file = '2016-11-11 13:53:27.835483.db'
+    problem_dir = 'generate_swiss_flag'
+    db_file = '2016-11-18 21:32:53.149436.db'
 
     THIS_FILE = os.path.abspath(__file__)
     RESULTS_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, 'problems', 'results'))
