e12469b17d4c4d2185a6952821e2ea287f762e22
Mathias Ose <mathias.ose@gmail.com>
2016-12-13 21:10:32 +0100

    Add 2D CA visualization helpers

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -1,5 +1,7 @@
 from typing import T, Tuple
 
+from visualization.colors import colormap, norm
+
 QUIESCENT = ' '
 ALPHABET_2 = (QUIESCENT, '■',)
 ALPHABET_3 = ALPHABET_2 + ('□',)
@@ -90,3 +92,32 @@ def pad_pattern(pattern: Tuple[Tuple[T]], dead_cell: T, n=1) -> Tuple[Tuple[T]]:
     padded_pattern += [(d,) * new_w] * n
 
     return tuple(padded_pattern)
+
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+
+    import seaborn
+
+    seaborn.set_style('white')
+
+    patterns = MOSAIC, BORDER, TRICOLOR, SWISS, NORWEGIAN
+
+    colors = {
+        ' ': 0,
+        '■': 1,
+        '□': 2,
+        '▨': 3,
+    }
+
+    for pattern in patterns:
+        fig = plt.figure()
+
+        plt.imshow(
+            [[colors.get(x) for x in row] for row in pattern],
+            interpolation='none',
+            cmap=colormap,
+            norm=norm,
+        )
+
+    plt.show()
---
+++ visualization/colors.py
@@ -0,0 +1,9 @@
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from seaborn.palettes import color_palette
+
+n_colors = 4
+colormap = ListedColormap(
+    color_palette(['#FFFFFF', '#e74c3c', '#95a5a6', '#34495e'], n_colors=n_colors),
+    N=n_colors
+)
+norm = BoundaryNorm(list(range(0, n_colors + 1)), n_colors)
