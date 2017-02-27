c2ea9b463e8e5b8fb48b58795c03afbfb0058404
Mathias Ose <mathias.ose@gmail.com>
2017-02-27 13:48:19 +0100

    Use celery_dill_serializer from pypi

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,28 +1,8 @@
-from dill import dill
-from kombu.serialization import pickle_loads, pickle_protocol, registry
-from kombu.utils.encoding import str_to_bytes
-
-
-def register_dill():
-    def encode(obj, dumper=dill.dumps):
-        return dumper(obj, protocol=pickle_protocol)
-
-    def decode(s):
-        return pickle_loads(str_to_bytes(s), load=dill.load)
-
-    registry.register(
-        name='dill',
-        encoder=encode,
-        decoder=decode,
-        content_type='application/x-python-serialize',
-        content_encoding='binary'
-    )
-
+from celery_dill_serializer import register_dill
 
 register_dill()
 
 task_serializer = 'dill'
-event_serializer = 'dill'
 result_serializer = 'dill'
 accept_content = ['dill']
 
